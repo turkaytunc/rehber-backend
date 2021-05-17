8fbf1bd48a2dfc5094b4557b0459676a89e94ea9
Türkay Tunç <turkaytunc00@gmail.com>
2021-05-17 20:47:50 +0300

    add: cors and helmet package

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,11 @@
 import express from 'express';
+import helmet from 'helmet';
+import cors from 'cors';
 
 const app = express();
+
+app.use(cors());
+app.use(helmet());
 
 app.get('/', (req, res, next) => {
   res.json({ message: 'Hello' });
