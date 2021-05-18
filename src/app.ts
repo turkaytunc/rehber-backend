f773feab8bdac800b14a89331643c27cf1ba4503
Türkay Tunç <turkaytunc00@gmail.com>
2021-05-18 07:15:15 +0300

    add: morgan logger

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import helmet from 'helmet';
 import cors from 'cors';
+import morgan from 'morgan';
+
 import { userRouter } from './routers';
 
 const app = express();
 
+app.use(morgan('dev'));
 app.use(cors());
 app.use(helmet());
 
